ab031792168f2ee2a4b28a23978a731acfaf87f5
Sarita Singh <saritasingh.0425@gmail.com>
2020-11-25 16:04:02 +0530

    Update minimum_and_maximum_differences.cpp

--- minimum_and_maximum_differences.cpp
+++ minimum_and_maximum_differences.cpp
@@ -1,69 +1,56 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-void swap_min(int i,int n){
-    for(i;i<=n;i++){
-        if(i%2) cout<<i+1<<" ";
-        else    cout<<i-1<<" ";
-    }
-}
 int main(){
 ios_base::sync_with_stdio(false);
 cin.tie(0);
 cout.tie(0);
 int n;
 cin>>n;
-//for minimum
-if(n%2){
-    int i=1;
-    if(n==3)    cout<<"4\n";
-    else{
-        int sum=n+2;
-        cout<<sum<<"\n";
-    }
-    while(i<n){
-        if(i==1){
-            cout<<i+2<<" "<<i<<" "<<i+1<<" ";
-            i+=3;
-        }
-        else{
-            swap_min(i,n);
-            break;
-        }
-    }
-    cout<<"\n";
-}
-else{
+if(n%2==0){
+    //for minimum
     cout<<n<<"\n";
-    int i=1;
-    swap_min(i,n);
-}
-//for maximum
-int mid=(n/2);
-int sum=0;
-for(int i=1;i<=n;i++){
-    if((n-i+1)>i){
-        sum+=(n-(2*i)+1);
-    }
-    else    sum+=((2*i)-n-1);
-}
-if(!n%2){
-    cout<<sum<<"\n";
-    for(int i=1;i<=n;i++)
-        cout<<(n-i+1)<<" ";
-}
-else{
-    cout<<sum-1<<"\n";
-    for(int i=1;i<mid;i++){
-        cout<<(n-i+1)<<" ";
-    }
-    cout<<mid+1<<" "<<mid<<" ";
-    for(int i=(mid+2);i<=n;i++){
-        cout<<(n-i+1)<<" ";
+    for(int i=1;i<=n;i++){
+        if (i%2)    cout<<i+1<<" ";
+        else cout<<i-1<<" ";
     }
     cout<<"\n";
-
+    //for maximum
+    int sum=0;
+    int mid=n/2;
+    for(int j=1;j<=mid;j++) sum+=(n-2*j+1);
+    sum=2*sum;
+    cout<<sum<<"\n";
+    for(int j=1;j<=n;j++){
+        cout<<n-j+1<<" ";
+    }
+    cout<<"\n";
 }
-
+else{
+    //for minimum
+    cout<<n+1<<"\n";
+    int i=1;
+    cout<<i+1<<" "<<i+2<<" "<<i<<" ";
+    for(int i=4;i<=n;i++){
+        if (i%2)    cout<<i-1<<" ";
+        else cout<<i+1<<" ";
+    }
+    cout<<"\n";
+    //for maximum
+    int m=n/2;
+    int sum=0;
+    for(int i=1;i<m;i++)
+        sum+=(n-2*i+1);
+    sum+=4;
+    for(int i=m+3;i<=n;i++)
+        sum+=(i-(n-i+1));
+    cout<<sum<<"\n";
+    for(int j=1;j<m;j++)
+        cout<<n-j+1<<" ";
+    cout<<m+2<<" "<<m<<" "<<m+1<<" ";
+    for(int j=m+3;j<=n;j++)
+        cout<<n-j+1<<" ";
+    cout<<"\n";
+}
 return 0;
 }
