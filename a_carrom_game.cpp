a7b61e6cd37a4bed3496028d7e0595f1edf76aaa
Sarita Singh <saritasingh.0425@gmail.com>
2020-11-26 09:47:59 +0530

    nov_circuits'20
    https://www.hackerearth.com/challenges/competitive/november-circuits-20/algorithm/interesting-carrom-game-f2e47518/

---
+++ a_carrom_game.cpp
@@ -0,0 +1,86 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+void input(vector<int> &a,int n){
+	for(int i=0;i<n;i++){
+		cin>>a[i];
+	}
+}
+int main(){
+ios_base::sync_with_stdio(false);
+cin.tie(0);
+cout.tie(0);
+int n;
+cin>>n;
+vector<int> black(n);
+vector<int> white(n);
+input(black,n);//hitted by A-C team
+input(white,n);
+int red;
+cin>>red;
+int a,b,c,d;//A,B,C,D
+cin>>a>>b>>c>>d;
+//order is:A-B-C-D-A-B...
+int i=0;
+int j=0;
+while(i<=n||j<=n){
+	if(i!=n){
+		if(a>=black[i])	{
+			i++;
+			a+=black[i];
+		}
+		else	a++;
+	}
+	else{
+		if(a>=red){
+			cout<<"A-C";
+			break;
+		}
+		else	a++;
+	}
+	if(j!=n){
+		if(b>=white[j])	{
+			j++;
+			b+=white[j];
+		}
+		else	b++;
+	}
+	else{
+		if(a>=red){
+			cout<<"B-D";
+			break;
+		}
+		else	b++;
+	}
+	if(i!=n){
+		if(c>=black[i])	{
+			i++;
+			c+=black[i];
+		}
+		else	c++;
+	}
+	else{
+		if(c>=red){
+			cout<<"A-C";
+			break;
+		}
+		else	c++;
+	}
+	if(j!=n){
+		if(d>=white[j])	{
+			j++;
+			d+=white[j];
+		}
+		else	d++;
+	}
+	else{
+		if(d>=red){
+			cout<<"B-D";
+			break;
+		}
+		else	d++;
+	}	
+}
+
+return 0;
+}
