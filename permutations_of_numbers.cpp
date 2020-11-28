dd158f33850e564be56f5da6f1caf59cd793ac7a
Sarita Singh <saritasingh.0425@gmail.com>
2020-11-28 20:40:19 +0530

    november circuits'20
    https://www.hackerearth.com/challenges/competitive/november-circuits-20/algorithm/deep-loves-permutation-9137f58f/

---
+++ permutations_of_numbers.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main(){
+ios_base::sync_with_stdio(false);
+cin.tie(0);
+cout.tie(0);
+int t,n;
+cin>>t;
+while(t--){
+    cin>>n;
+    vector<ll> L(n);
+    vector<ll> R(n);
+    for(int i=0;i<n;i++)    cin>>L[i];
+    for(int i=0;i<n;i++)    cin>>R[i];
+    for(int i=0;i<n;i++){
+        
+    }
+      
+}
+return 0;
+}
