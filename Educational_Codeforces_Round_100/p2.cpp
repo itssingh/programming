ced8ec4769d65ca2606f23a084528c07b57ed7c2
Sarita Singh <saritasingh.0425@gmail.com>
2020-12-19 13:30:43 +0530

    latest_update

---
+++ Educational_Codeforces_Round_100/p2.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main(){
+ios_base::sync_with_stdio(false);
+cin.tie(0);
+cout.tie(0);
+int t;
+cin>>t;
+while(t--){
+    int n;
+    ll temp;
+    
+ 
+}
+return 0;
+}
