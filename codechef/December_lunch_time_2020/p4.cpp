7ddeb7ae543f6e53ab220262fcd70585795b65b4
Sarita Singh <saritasingh.0425@gmail.com>
2020-12-27 01:09:36 +0530

    update

--- codechef/December_lunch_time_2020/p4.cpp
+++ codechef/December_lunch_time_2020/p4.cpp
@@ -1,14 +1,43 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+bool flag= true;
 int main(){
 ios_base::sync_with_stdio(false);
 cin.tie(0);
 cout.tie(0);
- t;
+int t;
 cin>>t;
 while(t--){
- 
+    int n,m,q,x,y,k;
+    ll v;
+    cin>>n>>m;
+    vector<vector<ll>> a;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++)
+            cin>>a[i][j];
+    }
+    cin>>q;
+    for(int it=0;it<q;it++){
+        flag =true;
+        cin>>x>>y>>v;
+        a[x-1][y-1]=v;
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if (j>=i && a[i][j]!=a[0][j-i]){
+                        flag =false;
+                        break;
+                }
+                else if(i>j && a[i][j]!=a[i-j][0]){
+                        flag =false;
+                        break;
+               }
+
+            }
+        }
+        if(flag)    cout<<"Yes\n";
+        else    cout<<"No\n";
+    }
 }
 return 0;
 }
