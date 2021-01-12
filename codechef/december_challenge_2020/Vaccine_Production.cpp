2c57827e2e743070e59b0ced52a02e93ed9ad8a4
Sarita Singh <saritasingh.0425@gmail.com>
2021-01-12 21:56:16 +0530

    https://www.codechef.com/problems/VACCINE1

--- codechef/december_challenge_2020/Vaccine_Production.cpp
+++ codechef/december_challenge_2020/Vaccine_Production.cpp
@@ -5,8 +5,7 @@ int main(){
 ios_base::sync_with_stdio(false);
 cin.tie(0);
 cout.tie(0);
-int d1,v1,d2,v2,p;
-int d=1;
+int d1,v1,d2,v2,p,d;
 cin>>d1>>v1>>d2>>v2>>p;
 if(d1>d2){
     int temp=d1;
@@ -16,35 +15,18 @@ if(d1>d2){
     v1=v2;
     v2=temp;
 }
-while(p>0){
-   if(d1!=d2){
-       if(d<d1){
-           d=d1;
-           p-=v1;
-       }
-       else if(d<d2){
-           d++;
-           p-=v1;
-       }
-       else{
-           int v=v1+v2;
-           v=ceil(p/v);
-           d+=v;
-           break;
-       }
-   }
-   else{
-       if(d<d1) {
-        d=d1;
-       p-=(v1+v2);}
-       else{
-          int v=v1+v2;
-           v=ceil(p/v);
-           d+=v;
-           break; 
-       }
-   }      
+if(d1==d2){
+    int v=v1+v2;
+    d=(d1-1)+((p-1+v)/v);
 }
-cout<<d;
+else{
+    if((p-1+v1)/v1<=d2-d1){
+        d=d1-1+(p-1+v1)/v1;
+    }
+    else{
+        d=d2-1+(p-((d2-d1)*v1)-1+v1+v2)/(v1+v2);
+    }
+}
+    cout<<d<<"\n";
 return 0;
 }
