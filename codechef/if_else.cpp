5158e5fade85a1318e2597f77a50a2928807561c
Sarita Singh <saritasingh.0425@gmail.com>
2021-01-14 15:36:20 +0530

    I am adding some more codes
    dded by Sarita

---
+++ codechef/if_else.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a=5;
+    if(a>3)
+        cout<<a<<" is greater than 3\n";
+    else
+        cout<<a<<" is smaller than 3\n";
+return 0;
+}
